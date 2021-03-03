04bcfb6335270267575eae905becbef0390da669
강희연 <the.onlyeon@gmail.com>
2021-03-03 10:37:11 +0900

    Resolve(core + product): 편집 화면 모달 레이아웃 추가 작업
    - 편집 화면 모달 레이아웃 추가
    - 공통 컴포넌트 form, modal, select, tab, upload 등 수정

--- src/js/studio.js
+++ src/js/studio.js
@@ -34,3 +34,66 @@ const requirePngAll = (r) => {
 }
 requirePngAll(require.context('../style-core/images/common/', true, /\.png$/));
 requirePngAll(require.context('../style-product/images/common/', true, /\.png$/));
+
+// ****************************
+// UI Event
+// ****************************
+
+// toggle variation
+const beforeSelectElement = document.querySelectorAll(".toggle-switch__switch, .toggle-button__item");
+const afterSelectSuffix = "--selected";
+const beforeOpenElement = document.querySelectorAll(".accordion__head, .select");
+const afterOpenSuffix = "--open";
+
+// 토글 버튼 클릭시 --selected 추가
+const toggleSelect = () => {
+
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+  }
+
+  beforeSelectElement.forEach((i) => {
+
+    i.addEventListener('click', (event) => {
+      event.preventDefault();
+      let afterSelectButton = i.classList[0].concat(afterSelectSuffix);
+
+      // Test
+      console.log(`Click Element => ${i.classList[0]}`);
+      console.log(`Element + "--selected" => ${afterSelectButton}`);
+
+      i.classList.toggle(afterSelectButton);
+
+    })
+  })
+}
+
+// 열리기 class 추가
+const toggleOpen = () => {
+
+  if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+  }
+
+  beforeOpenElement.forEach((i) => {
+
+    i.addEventListener('click', (event) => {
+      event.preventDefault();
+      let afterOpenButton = i.classList[0].concat(afterOpenSuffix);
+
+      // Test
+      console.log(`Click Element => ${i.classList[0]}`);
+      console.log(`Element + "--open" => ${afterOpenButton}`);
+
+      i.classList.toggle(afterOpenButton);
+
+    })
+  })
+}
+
+const init = () => {
+  toggleSelect();
+  toggleOpen();
+}
+
+init();
